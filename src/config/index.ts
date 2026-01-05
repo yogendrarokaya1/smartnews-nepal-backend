bde1999c2326e8180305b3e8a8a83a7aef8b08f3
yogendrarokaya1 <yogendrarokaya@gmail.com>
2026-01-05 19:07:40 +0545

    controller added

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT: number = Number(process.env.PORT) || 5000;
-export const MONGO_URI: string = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/smartnews";
+export const MONGODB_URI: string = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/smartnews";
 export const JWT_SECRET: string = process.env.JWT_SECRET || "default";
