5af6f379806924700add19f30a187fd3fc626259
yogendrarokaya1 <yogendrarokaya@gmail.com>
2026-02-10 16:02:36 +0545

    admin controller for get all users added

--- src/controllers/admin/admin.controller.ts
+++ src/controllers/admin/admin.controller.ts
@@ -2,9 +2,9 @@ import { CreateUserDTO, LoginUserDTO, UpdateUserDTO } from "../../dtos/user.dto"
 import { Request, Response, NextFunction } from "express";
 import z from "zod";
 import { AdminUserService } from "../../services/admin/admin.service";
+import { QueryParams } from "../../types/query.type";
 
 let adminUserService = new AdminUserService();
-
 export class AdminUserController {
     async createUser(req: Request, res: Response, next: NextFunction) {
         try {
@@ -14,7 +14,7 @@ export class AdminUserController {
                     { success: false, message: z.prettifyError(parsedData.error) }
                 )
             }
-            if(req.file){   
+            if (req.file) {
                 parsedData.data.imageUrl = `/uploads/${req.file.filename}`;
             }
             const userData: CreateUserDTO = parsedData.data;
@@ -31,9 +31,12 @@ export class AdminUserController {
 
     async getAllUsers(req: Request, res: Response, next: NextFunction) {
         try {
-            const users = await adminUserService.getAllUsers();
+            const { page, size, search }: QueryParams = req.query;
+            const { users, pagination } = await adminUserService.getAllUsers(
+                page, size, search
+            );
             return res.status(200).json(
-                { success: true, data: users, message: "All Users Retrieved" }
+                { success: true, data: users, pagination: pagination, message: "All Users Retrieved" }
             );
         } catch (error: Error | any) {
             return res.status(error.statusCode ?? 500).json(
@@ -44,15 +47,15 @@ export class AdminUserController {
 
     async updateUser(req: Request, res: Response, next: NextFunction) {
         try {
-            const userId = req.params.id;
+            const userId = req.params.id as string;
             const parsedData = UpdateUserDTO.safeParse(req.body); // validate request body
             if (!parsedData.success) { // validation failed
                 return res.status(400).json(
                     { success: false, message: z.prettifyError(parsedData.error) }
                 )
             }
-            
-            if(req.file){   
+
+            if (req.file) {
                 parsedData.data.imageUrl = `/uploads/${req.file.filename}`;
             }
             const updateData: UpdateUserDTO = parsedData.data;
@@ -70,7 +73,7 @@ export class AdminUserController {
 
     async deleteUser(req: Request, res: Response, next: NextFunction) {
         try {
-            const userId = req.params.id;
+            const userId = req.params.id as string;
             const deleted = await adminUserService.deleteUser(userId);
             if (!deleted) {
                 return res.status(404).json(
@@ -89,7 +92,7 @@ export class AdminUserController {
 
     async getUserById(req: Request, res: Response, next: NextFunction) {
         try {
-            const userId = req.params.id;
+            const userId = req.params.id as string;
             const user = await adminUserService.getUserById(userId);
             return res.status(200).json(
                 { success: true, data: user, message: "Single User Retrieved" }
--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -10,3 +10,14 @@ export async function connectDatabase(){
         process.exit(1); // Exit process with failure
     }
 }
+
+
+export async function connectDatabaseTest(){
+    try {
+        await mongoose.connect(MONGODB_URI + "_test");
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Database Error:", error);
+        process.exit(1); // Exit process with failure
+    }
+}
