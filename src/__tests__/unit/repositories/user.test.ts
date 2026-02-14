b74b925912490a7c18a52f5d1a2f8085cddf1ab3
yogendrarokaya1 <yogendrarokaya@gmail.com>
2026-02-14 18:09:29 +0545

    user test added

--- src/__tests__/unit/repositories/user.test.ts
+++ src/__tests__/unit/repositories/user.test.ts
@@ -1,30 +1,30 @@
-// import { UserRepository } from "../../../repositories/user.repository";
-// import { UserModel } from "../../../models/user.model";
-// import mongoose from "mongoose";
+import { UserRepository } from "../../../repositories/user.repository";
+import { UserModel } from "../../../models/user.model";
+import mongoose from "mongoose";
 
-// describe('User Repository Unit Tests', () => {
-//     let userRepository: UserRepository;
+describe('User Repository Unit Tests', () => {
+    let userRepository: UserRepository;
 
-//     beforeAll(() => {
-//         userRepository = new UserRepository();
-//     });
-//     afterEach(async () => {
-//         await UserModel.deleteMany({});
-//     });
+    beforeAll(() => {
+        userRepository = new UserRepository();
+    });
+    afterEach(async () => {
+        await UserModel.deleteMany({});
+    });
 
-//     afterAll(async () => {
-//         await mongoose.connection.close();
-//     });
-//     test('should create a new user', async () => {
-//         const userData = {
-//             phoneNumber: '9848765432',
-//             email: 'test@example.com',
-//             password: 'Password123!',
-//             fullName: 'Test User',
-//         };
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+    test('should create a new user', async () => {
+        const userData = {
+            phoneNumber: '9848765432',
+            email: 'test@example.com',
+            password: 'Password123!',
+            fullName: 'Test User',
+        };
 
-//         const newUser = await userRepository.createUser(userData);
-//         expect(newUser).toBeDefined();
-//         expect(newUser.email).toBe(userData.email);
-//     });
-// });
+        const newUser = await userRepository.createUser(userData);
+        expect(newUser).toBeDefined();
+        expect(newUser.email).toBe(userData.email);
+    });
+});
