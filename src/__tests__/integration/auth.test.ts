e474e85a6412f76658380bfd8c91b4552dc897c8
yogendrarokaya1 <yogendrarokaya@gmail.com>
2026-02-14 18:10:01 +0545

    auth test done

--- src/__tests__/integration/auth.test.ts
+++ src/__tests__/integration/auth.test.ts
@@ -1,67 +1,67 @@
-// import request from 'supertest';
-// import app from '../../app';
-// import { UserModel } from '../../models/user.model';
+import request from 'supertest';
+import app from '../../app';
+import { UserModel } from '../../models/user.model';
 
-// describe('Auth Integration Tests', () => {
-//     const testUser = {
-//         phoneNumber: '9848765432',
-//         email: 'test@example.com',
-//         password: 'Password123',
-//         confirmPassword: 'Password123',
-//         fullName: 'Test User',
-//     };
+describe('Auth Integration Tests', () => {
+    const testUser = {
+        phoneNumber: '9848765432',
+        email: 'test@example.com',
+        password: 'Password123',
+        confirmPassword: 'Password123',
+        fullName: 'Test User',
+    };
 
-//     beforeAll(async () => {
-//         // Any setup before tests run can be done here
-//         await UserModel.deleteMany({
-//             $or: [{ email: testUser.email }]
-//         });
-//     });
+    beforeAll(async () => {
+        // Any setup before tests run can be done here
+        await UserModel.deleteMany({
+            $or: [{ email: testUser.email }]
+        });
+    });
 
-//     afterAll(async () => {
-//         await UserModel.deleteMany({
-//             $or: [{ email: testUser.email }]
-//         });
-//     });
+    afterAll(async () => {
+        await UserModel.deleteMany({
+            $or: [{ email: testUser.email }]
+        });
+    });
 
-//     describe('POST /api/auth/register', () => {
-//         test('should register a new user', async () => {
-//             const response = await request(app)
-//                 .post('/api/auth/register')
-//                 .send(testUser)
+    describe('POST /api/auth/register', () => {
+        test('should register a new user', async () => {
+            const response = await request(app)
+                .post('/api/auth/register')
+                .send(testUser)
             
-//             expect(response.status).toBe(201);
-//             expect(response.body).toHaveProperty('success', true);
-//         })
+            expect(response.status).toBe(201);
+            expect(response.body).toHaveProperty('success', true);
+        })
 
-//         test('should not register a new user with duplicate email', async () => {
-//             const response = await request(app)
-//                 .post('/api/auth/register')
-//                 .send(testUser)
+        test('should not register a new user with duplicate email', async () => {
+            const response = await request(app)
+                .post('/api/auth/register')
+                .send(testUser)
             
-//             expect(response.status).toBe(403);
-//             expect(response.body).toHaveProperty('success', false);
-//         })
+            expect(response.status).toBe(403);
+            expect(response.body).toHaveProperty('success', false);
+        })
 
-//     });
+    });
 
-//     describe('POST /api/auth/login', () => {
+    describe('POST /api/auth/login', () => {
 
-//         test('should login an existing user', async () => {
-//             const response = await request(app)
-//                 .post('/api/auth/login')
-//                 .send({ email: testUser.email, password: testUser.password });
-//             expect(response.status).toBe(200);
-//             expect(response.body).toHaveProperty('success', true);
-//             expect(response.body).toHaveProperty('token');
-//         });
+        test('should login an existing user', async () => {
+            const response = await request(app)
+                .post('/api/auth/login')
+                .send({ email: testUser.email, password: testUser.password });
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveProperty('success', true);
+            expect(response.body).toHaveProperty('token');
+        });
 
-//         test('should not login with incorrect password', async () => {
-//             const response = await request(app)
-//                 .post('/api/auth/login')
-//                 .send({ email: testUser.email, password: 'WrongPassword!' });
-//             expect(response.status).toBe(401);
-//             expect(response.body).toHaveProperty('success', false);
-//         });
-//     });
-// });
+        test('should not login with incorrect password', async () => {
+            const response = await request(app)
+                .post('/api/auth/login')
+                .send({ email: testUser.email, password: 'WrongPassword!' });
+            expect(response.status).toBe(401);
+            expect(response.body).toHaveProperty('success', false);
+        });
+    });
+});
